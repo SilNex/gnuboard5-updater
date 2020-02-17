9845f8ac09eab99a81976feb1721134b1d9b9fd6
silnex <silnex@silnex.kr>
2020-02-17 16:13:58 +0900

    파일 하나로 합침 어차피 레거신데 뭐

--- gnuUpConfig.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('__GNU_DIR__', __DIR__ . '/html/');
-define('__PATCH_DIR__', __DIR__ . '/patch/');
-define('__ORIGIN_DIR__', __DIR__ . '/origin/');
-define('__BACKUP_DIR__', __DIR__ . '/backup/');
