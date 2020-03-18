d129b135b3e3020383f8293bff1efbe28978aa59
silnex <silnex@silnex.kr>
2020-03-18 21:28:53 +0900

    Add next method

--- src/SIRUpdater/VersionManager.php
+++ src/SIRUpdater/VersionManager.php
@@ -17,6 +17,7 @@ class VersionManager implements VersionManagerInterface
     protected $versionList = [];
     protected $paser;
     protected $basePath, $publicPath;
+    protected $current;
 
     public function __construct(string $publicPath, string $type = 'Gnuboard')
     {
@@ -24,19 +25,47 @@ class VersionManager implements VersionManagerInterface
         $this->basePath = $publicPath . '..' . DIRECTORY_SEPARATOR;
 
         if (!is_dir($publicPath)) {
-            throw new Exception("경로가 잘못되었습니다.");
-        } elseif (file_exists($publicPath . 'config.php')) {
-            throw new Exception("config.php 파일을 찾을 수 없습니다.");
+            throw new Exception("경로가 잘못되었습니다.\n");
+        } elseif (file_exists($this->publicPath . 'config.php')) {
+            throw new Exception("config.php 파일을 찾을 수 없습니다.\n");
         }
+
+        $this->current = $this->getCurrentVersion();
 
         if ($type === 'Gnuboard') {
             $this->parser = new GnuboardParserFactory();
         } elseif ($type === 'YoungCart') {
             // $this->parser = new YoungCartParserFactory();
-            throw new Exception('영카트는 현재 지원되지 않습니다.');
+            throw new Exception("영카트는 현재 지원되지 않습니다.\n");
         } else {
-            throw new InvalidArgumentException("type은 [Gnuboard, YoungCart]만 허용 됩니다.");
+            throw new InvalidArgumentException("type은 [Gnuboard, YoungCart]만 허용 됩니다.\n");
         }
         $this->versionList = $this->parser->getPostList();
+    }
+
+    protected function getCurrentVersion()
+    {
+        $configFile = file_get_contents($this->publicPath . 'config.php');
+        preg_match('/\(\'G5_GNUBOARD_VER\',\s\'([0-9.]+)\'\)\;/', $configFile, $match);
+        if (isset($match[1])) {
+            return $match[1];
+        } else {
+            throw new Exception("config 파일에서 버전을 찾을 수 없습니다.");
+        }
+    }
+
+    public function current()
+    {
+        return $this->current;
+    }
+
+    public function next()
+    {
+        
+    }
+
+    public function previous()
+    {
+        
     }
 }
