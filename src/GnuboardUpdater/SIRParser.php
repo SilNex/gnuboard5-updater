570cba04bb09c702feea132fdc8e07c352025481
silnex <silnex@silnex.kr>
2020-03-18 16:53:24 +0900

    Make SIRParser

--- src/GnuboardUpdater/SIRParser.php
+++ src/GnuboardUpdater/SIRParser.php
@@ -2,30 +2,100 @@
 
 namespace silnex\GnuboardUpdater;
 
+use IntlException;
+use InvalidArgumentException;
 use silnex\Util\Curl;
 
 class SIRParser
 {
     protected $sirBoardPattern = '/<a\shref=\"(.*)\"\sclass="title_link">\s+\[?(보안패치|정식버전|베타버전)?\]?\s?그누보드\s?(5\.4\.[0-9]\.?[0-9]?)/';
-    protected $sirAttachPattern = '/onclick=\"file_download\(\'(\/\/sir\.kr\/bbs\/download\.php.*)\',\s\'gnuboard5\.4\.[0-9]\.?[0-9]?\.patch\.tar\.gz\'\);\"/';
+    protected $sirAttachPattern = '/onclick=\"file_download\(\'(\/\/sir\.kr\/bbs\/download\.php.*)\',\s\'gnuboard5\.4\.[0-9]\.?[0-9]?(\.patch)?\.tar\.gz\'\);\"/';
     protected $githubUriPattern = '/>(https:\/\/github\.com\/gnuboard\/gnuboard5\/commit\/[a-z0-9]+)</';
+
+    protected $postList = [];
+    protected $postAttach = [];
 
     public function __construct()
     {
         $this->url = "https://sir.kr/g5_pds";
         $this->curl = new Curl();
+        $this->postList = $this->parsePostList();
+        ksort($this->postList);
+    }
+
+    protected function versionFormat(string $versionString)
+    {
+        if (preg_match('/^(5\.4\.[0-9]\.[0-9])$/', $versionString)) {
+            return $versionString;
+        } elseif (preg_match('/^(5\.4\.[0-9])$/', $versionString)) {
+            return $versionString . '.0';
+        } else {
+            throw new InvalidArgumentException("{$versionString}은 정상적인 포맷의 버전이 아닙니다.");
+        }
+    }
+
+    protected function matches(string $pattern, ?string $url, string $response = null)
+    {
+        if (isset($url) && is_null($response)) {
+            $response = $this->get($url);
+        }
+        preg_match_all($pattern, $response, $mataches);
+        return $mataches;
+    }
+
+    protected function parsePostList()
+    {
+        $posts = $this->matches($this->sirBoardPattern, $this->url);
+        for ($i = 0; $i < count($posts[0]); $i++) {
+            try {
+                $version = $this->versionFormat($posts[3][$i]);
+            } catch (InvalidArgumentException $e) {
+                die($e->getMessage());
+            }
+
+            $this->postList[$version] = [
+                'href' => "https:" . $posts[1][$i],
+                'type' => $posts[2][$i],
+                'version' => $version,
+            ];
+        }
+        return $this->postList;
+    }
+
+    protected function get(string $url)
+    {
+        return $this->curl->get($url);
+    }
+
+    public function getPostAttachFiles(string $version)
+    {
+        if ($version < '5.4.0.0') {
+            throw new InvalidArgumentException("그누보드 버전 5.4 이상만 지원합니다.");
+        } elseif (!preg_match('/^(5\.4\.[0-9]\.?[0-9]?)$/', $version)) {
+            throw new InvalidArgumentException("버전은 5.4.x.x 와 같은 양식으로 입력해야합니다.");
+        }
+
+        $attachFiles = $this->matches($this->sirAttachPattern, $this->postList[$version]['href']);
+
+        for ($i = 0; $i < 2; $i++) {
+            $downloadLink = 'https:' . str_replace('download', 'download2', html_entity_decode($attachFiles[1][$i]));
+            if (strpos($attachFiles[0][$i], 'patch')) {
+                $patchLink = $downloadLink;
+            } else {
+                $fullLink = $downloadLink;
+            }
+        }
+
+        return [
+            $version => [
+                'patch' => $patchLink,
+                'full' => $fullLink,
+            ],
+        ];
     }
 
     public function getPostList()
     {
-        $response = $this->curl->get($this->url)->toString();
-        preg_match_all($this->sirBoardPattern, $response, $posts);
-        for ($i = 0; $i < count($posts[0]); $i++) {
-            $postList[] = [
-                'href' => "https:" . $posts[1][$i],
-                'info' => $posts[2][$i],
-                'version' => $posts[3][$i]
-            ];
-        }
+        return $this->postList;
     }
 }
--- tests/unit/parser.php
+++ tests/unit/parser.php
@@ -3,4 +3,4 @@
 use silnex\GnuboardUpdater\SIRParser;
 
 $parser = new SIRParser;
-var_dump($parser->getPostList());
+var_dump($parser->getPostAttachFiles('5.4.1.0'));
