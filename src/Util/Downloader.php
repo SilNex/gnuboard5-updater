e5c70b9289b2f3fac333a21621bb42dd0dac6433
silnex <silnex@silnex.kr>
2020-03-19 02:40:14 +0900

    Add check dir empty

--- src/Util/Downloader.php
+++ src/Util/Downloader.php
@@ -10,17 +10,19 @@ class Downloader
     {
         if (!is_dir($storePath)) {
             mkdir($storePath, 0777, true);
+        } elseif (count(scandir($storePath)) !== 2) {
+            throw new Exception("{$storePath}는 비어있는 폴더가 아닙니다.\n");
         }
 
         $filePath = $storePath . '/' . $fileName;
         if (file_exists($filePath)) {
-            throw new Exception("이미 파일이 존재합니다.");
+            throw new Exception("{$filePath}파일이 이미 존재합니다.\n");
         }
 
         $curl = new Curl;
         $data = $curl->get($downloadLink);
         if (!file_put_contents($storePath . '/' . $fileName, $data)) {
-            throw new Exception("{$filePath}에 저장을 실패했습니다");
+            throw new Exception("{$filePath}에 저장을 실패했습니다.\n");
         }
     }
 }
